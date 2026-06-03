99a8df6dad4bdf5b87ab43ac794a491f24beacd7
RottenMeat2599 <leomord.a@yandex.ru>
2026-06-03 10:40:36 +0300

    Add files via upload

---
+++ laba4_Alg.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <sstream>
+
+int main() {
+    std::string text;
+
+    std::cout << "Enter the text: ";
+    std::getline(std::cin, text);
+
+    std::map<std::string, int> wordCount;
+    std::stringstream ss(text);
+    std::string word;
+
+    while (ss >> word) {
+        wordCount[word]++;
+    }
+
+    std::cout << "\n";
+    int maxCount = 0;
+    std::string mostFrequentWord;
+
+    for (auto pair : wordCount) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+        if (pair.second > maxCount) {
+            maxCount = pair.second;
+            mostFrequentWord = pair.first;
+        }
+    }
+
+    if (!wordCount.empty()) {
+        std::cout << "\n";
+        std::cout << "The most frequent word: " << mostFrequentWord << ": " << maxCount;
+    }
+
+    return 0;
+}
